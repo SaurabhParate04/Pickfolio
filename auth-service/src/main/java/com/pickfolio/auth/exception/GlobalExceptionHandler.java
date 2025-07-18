773af982bbfee482372c1a930d12347f400df323
Saurabh Parate <sparate04@gmail.com>
2025-07-18 21:17:42 +0530

    Added SecurityConfig required by spring-security-starter, Fixed missing annotation in global exception handler

--- auth-service/src/main/java/com/pickfolio/auth/exception/GlobalExceptionHandler.java
+++ auth-service/src/main/java/com/pickfolio/auth/exception/GlobalExceptionHandler.java
@@ -5,7 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
